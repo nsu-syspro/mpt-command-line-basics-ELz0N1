922373d90eb8e39ba5b392598a4466c58de4afe1
Danil Zhukov <zhukov.danila2004@yandex.ru>
2024-04-24 01:04:15 +0700

    +rm tmp

--- solution/task1.sh
+++ solution/task1.sh
@@ -4,3 +4,4 @@ mktemp -d ./data/tmp
 tar -xzvf ./data/archive-part1.tar -C ./data/tmp
 unzip ./data/archive-part2.zip -d ./data/tmp
 tar -czvf ./data/archive-combined.tar.gz -C ./data/tmp .
+rm -rf ./data/tmp
