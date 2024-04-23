af311dbe584d2c5bd6a8999b7d36f38a56f9e105
Danil Zhukov <zhukov.danila2004@yandex.ru>
2024-04-23 23:30:20 +0700

    fixed solution

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
 mktemp -d ./data/tmp
 tar -xzvf ./data/archive-part1.tar -C ./data/tmp
 unzip ./data/archive-part2.zip -d ./data/tmp
-tar -czvf ./data/archive-combined.tar.gz ./data/tmp
+tar -czvf ./data/archive-combined.tar.gz -C ./data/tmp .
